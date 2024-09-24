ae1b032b222268ab097f457f702501eda95a9020
edw <it.ralphedwardgesguerra@gmail.com>
2024-09-24 20:19:38 +0800

    add GetListingCheque

---
+++ RDF.Arcana.API/Features/ListingFee To Cheque Conversion/GetListingCheque.cs
@@ -0,0 +1,9 @@
+﻿//using RDF.Arcana.API.Common.Pagination;
+
+//namespace RDF.Arcana.API.Features.ListingFee_To_Cheque_Conversion
+//{
+//    public class GetListingCheque
+//    {
+//        public class GetListingChequeQuery : IRequest<UserParams, PagedList<>>
+//    }
+//}
