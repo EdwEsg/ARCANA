03cf43ae01fd1ddef259cd4463f3c160948befc5
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-09-30 16:47:14 +0800

    ongoing SAReports

---
+++ RDF.Arcana.API/Features/Reports/SAReports.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RDF.Arcana.API.Features.Reports
+{
+    public class SAReports
+    {
+        public class SAReportsCommand : IRequest<IActionResult>
+        {
+            public int MyProperty { get; set; }
+        }
+    }
+}
