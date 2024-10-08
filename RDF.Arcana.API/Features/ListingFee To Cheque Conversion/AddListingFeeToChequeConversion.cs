dadcc3ca85351da2d23d47e52d6bb75537115bf8
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-10-08 16:55:44 +0800

    ongoing lf to check

--- RDF.Arcana.API/Features/ListingFee To Cheque Conversion/AddListingFeeToChequeConversion.cs
+++ RDF.Arcana.API/Features/ListingFee To Cheque Conversion/AddListingFeeToChequeConversion.cs
@@ -1,5 +1,6 @@
 ﻿//using RDF.Arcana.API.Common;
 //using RDF.Arcana.API.Data;
+//using RDF.Arcana.API.Domain;
 
 //namespace RDF.Arcana.API.Features.ListingFee_To_Cheque_Conversion
 //{
@@ -7,7 +8,11 @@
 //    {
 //        public class AddListingFeeToChequeConversionCommand : IRequest<Result>
 //        {
-//            public int ListingFeeId { get; set; }
+//            public int ClientId { get; set; }
+//            public string Bank { get; set; }
+//            public string ChequeNo { get; set; }
+//            public string ChequeDate { get; set; }
+//            public string VoucherNo { get; set; }
 //            public decimal AmountToConvert { get; set; }
 //        }
 
@@ -21,18 +26,64 @@
 
 //            public async Task<Result> Handle(AddListingFeeToChequeConversionCommand request, CancellationToken cancellationToken)
 //            {
-//                var listingFeeId = await _context.ListingFees
-//                    .Where(lf => lf.Status == Status.Approved && lf.Id == request.ListingFeeId)
-//                    .OrderByDescending(lf => lf.Total)
+//                var listingFees = await _context.ListingFees
+//                    .Where(lf => lf.Status == Status.Approved && 
+//                                 lf.ClientId == request.ClientId &&
+//                                 lf.IsActive &&
+//                                 lf.Total > 0)
+//                    .OrderBy(cr => cr.CratedAt)
 //                    .ToListAsync(cancellationToken);
 
-//                if (listingFeeId is null)
+//                if (listingFees is null)
 //                {
 //                    return ListingFeeToChequeConversionError.NotFound();
 //                }
 
+//                var listingFeesTotal = listingFees.Sum(t => t.Total);
+
 //                var amountToConvert = request.AmountToConvert;
-//                listingFeeId.First().Total -= amountToConvert;
+
+//                var checkBalance = listingFeesTotal - amountToConvert;
+
+//                if (checkBalance < 0)
+//                {
+//                    return ListingFeeToChequeConversionError.Insufficient();
+//                }
+
+//                foreach (var fee in listingFees)
+//                {
+//                    var remainingBalance = fee.Total - amountToConvert;
+
+//                    if (remainingBalance < 0) 
+//                    {
+//                        fee.Total = 0;
+//                        amountToConvert = Math.Abs(remainingBalance);
+                        
+//                    }
+//                    else
+//                    {
+//                        fee.Total = remainingBalance;
+//                        await _context.SaveChangesAsync(cancellationToken);
+//                        break;
+//                    }
+                    
+//                    await _context.SaveChangesAsync(cancellationToken);
+//                }
+
+//                var cheque = new Cheque
+//                {
+//                    ClientId = request.ClientId,
+//                    ListingFeeId = listingFees.First().Id,
+//                    PaymentTransactionId = null,
+//                    Bank = request.Bank,
+//                    ChequeNo = request.ChequeNo,
+//                    ChequeDate = request.ChequeDate,
+//                    VoucherNo = request.VoucherNo,
+//                    Amount = request.AmountToConvert,
+//                    RemainingBalance = 0,
+
+
+//                };
 
 //            }
 //        }
--- RDF.Arcana.API/Features/ListingFee To Cheque Conversion/ListingFeeToChequeConversionError.cs
+++ RDF.Arcana.API/Features/ListingFee To Cheque Conversion/ListingFeeToChequeConversionError.cs
@@ -5,5 +5,6 @@ namespace RDF.Arcana.API.Features.ListingFee_To_Cheque_Conversion
     public class ListingFeeToChequeConversionError
     {
         public static Error NotFound() => new("NotFound", "Listing Fee Not Found/Check if Status is Approved");
+        public static Error Insufficient() => new("Insufficient", "Listing Fee Insufficient Balance");
     }
 }
