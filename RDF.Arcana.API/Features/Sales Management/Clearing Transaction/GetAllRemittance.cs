b3839066e77316b7e95db54d4f29bdcbe10434d3
edw <it.ralphedwardgesguerra@gmail.com>
2024-10-27 21:03:03 +0800

    getRemittance

--- RDF.Arcana.API/Features/Sales Management/Clearing Transaction/GetAllRemittance.cs
+++ RDF.Arcana.API/Features/Sales Management/Clearing Transaction/GetAllRemittance.cs
@@ -1,56 +1,86 @@
-﻿//using RDF.Arcana.API.Common;
-//using RDF.Arcana.API.Common.Pagination;
-//using RDF.Arcana.API.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using RDF.Arcana.API.Common.Extension;
+using RDF.Arcana.API.Common.Pagination;
+using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Data;
 
-//namespace RDF.Arcana.API.Features.Sales_Management.Clearing_Transaction
-//{
-//    public class GetAllRemittance
-//    {
-//        public class GetAllRemittanceQuery : UserParams, IRequest<PagedList<Unit>>
-//        {
-//            public string Search { get; set; }
-//            public string Status { get; set; }
-//            public int? ClusterId { get; set; }
-//            public int AddedBy { get; set; }
-//            public string PaymentMethod { get; set; }
-//        }
+namespace Arcana.API.Features.Sales_Management.Clearing.Transaction
+{
+    [Route("api/sandbox")]
+    [ApiController]
+    public class GetAllRemittance : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public GetAllRemittance(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
 
-//        public class GetAllRemittanceResult
-//        {
-//            public string BusinessName { get; set; }
-//            public string OwnersName { get; set; }
-//            public string PaymentMethod { get; set; }
-//            public string PaymentChannel { get; set; }
-//            public string ReferenceNo { get; set; }
-//            public decimal TotalPaymentAmount { get; set; }
-//            public string Reason { get; set; }
-//            public string ATag { get; set; }
-//            public DateTime Date { get; set; }
-//            public ICollection<Invoice> Invoices { get; set; }
-//            public class Invoice
-//            {
-//                public string InvoiceNo { get; set; }
-//            }
-//        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetAllRemittanceQuery query)
+        {
+            try
+            {
+                var transactions = await _mediator.Send(query);
 
-//        public class Handler : IRequestHandler<GetAllRemittanceQuery, PagedList<GetAllRemittanceResult>>
-//        {
-//            private readonly ArcanaDbContext _context;
-//            public Handler(ArcanaDbContext context)
-//            {
-//                _context = context;
-//            }
+                Response.AddPaginationHeader(transactions.CurrentPage, transactions.PageSize, transactions.TotalCount,
+                    transactions.TotalPages, transactions.HasNextPage, transactions.HasPreviousPage);
 
-//            public Task<PagedList<GetAllRemittanceResult>> Handle(GetAllRemittanceQuery request, CancellationToken cancellationToken)
-//            {
-//                var paymentTransactions = _context.PaymentTransactions
-//                    .Where(pt => pt.PaymentRecord.Status == request.Status &&
-//                             pt.Transaction.Status != Status.Cancelled &&
-//                             pt.Transaction.Status != Status.Voided &&
-//                             pt.Status != Status.Voided &&
-//                             pt.Status != Status.Cancelled &&
-//                             pt.TotalAmountReceived > 0);
-//            }
-//        }
-//    }
-//}
+                var result = new
+                {
+                    transactions,
+                    transactions.CurrentPage,
+                    transactions.PageSize,
+                    transactions.TotalCount,
+                    transactions.TotalPages,
+                    transactions.HasNextPage,
+                    transactions.HasPreviousPage
+                };
+
+                var successResult = Result.Success(result);
+                return Ok(successResult);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+
+    public class GetAllRemittanceQuery : UserParams, IRequest<PagedList<GetAllRemittanceResult>>
+    {
+        public int TransactionId { get; set; }
+    }
+
+    public class GetAllRemittanceResult
+    {
+        public string BusinessName { get; set; }
+        public string Status { get; set; }
+        public string InvoiceNo { get; set; }
+        public string InvoiceType { get; set; }
+    }
+
+    public class Handler : IRequestHandler<GetAllRemittanceQuery, PagedList<GetAllRemittanceResult>>
+    {
+        private readonly ArcanaDbContext _context;
+        public Handler(ArcanaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PagedList<GetAllRemittanceResult>> Handle(GetAllRemittanceQuery request, CancellationToken cancellationToken)
+        {
+            var query = from transact in _context.Transactions
+                        where transact.Status == Status.Paid
+                        select new GetAllRemittanceResult
+                        {
+                            BusinessName = transact.Client.BusinessName,
+                            Status = transact.Status,
+                            InvoiceNo = transact.InvoiceNo,
+                            InvoiceType = transact.InvoiceType,
+                        };
+
+            return await PagedList<GetAllRemittanceResult>.CreateAsync(query, request.PageNumber, request.PageSize);
+        }
+    }
+}
