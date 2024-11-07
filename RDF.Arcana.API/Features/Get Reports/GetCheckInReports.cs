9fdf2dece3367549025e8f9f282f465fbd1555a6
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-11-07 13:47:21 +0800

    checkIn reports

---
+++ RDF.Arcana.API/Features/Get Reports/GetCheckInReports.cs
@@ -0,0 +1,122 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Common.Extension;
+using RDF.Arcana.API.Common.Pagination;
+using RDF.Arcana.API.Data;
+using static RDF.Arcana.API.Features.CheckIns.GetCheckIn;
+
+namespace RDF.Arcana.API.Features.Get_Reports
+{
+    [Route("api/get-check-in-reports"), ApiController]
+    public class GetCheckInReports : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public GetCheckInReports(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetCheckInReportsQuery query)
+        {
+            try
+            {
+                var checkIn = await _mediator.Send(query);
+
+                Response.AddPaginationHeader(
+                    checkIn.CurrentPage,
+                    checkIn.PageSize,
+                    checkIn.TotalCount,
+                    checkIn.TotalPages,
+                    checkIn.HasNextPage,
+                    checkIn.HasPreviousPage);
+                var result = new
+                {
+                    checkIn,
+                    checkIn.CurrentPage,
+                    checkIn.PageSize,
+                    checkIn.TotalCount,
+                    checkIn.TotalPages,
+                    checkIn.HasNextPage,
+                    checkIn.HasPreviousPage
+                };
+
+                var successResult = Result.Success(result);
+
+                return Ok(successResult);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        public class GetCheckInReportsQuery : UserParams, IRequest<PagedList<GetCheckInReportsResult>>
+        {
+            public int? AddedBy { get; set; }
+            public DateTime DateFrom { get; set; }
+            public DateTime DateTo { get; set; }
+        }
+
+        public class GetCheckInReportsResult
+        {
+            public string BusinessName { get; set; }
+            public string FullName { get; set; }
+            public string HouseNo { get; set; }
+            public string StreetName { get; set; }
+            public string Barangay { get; set; }
+            public string City { get; set; }
+            public string Province { get; set; }
+            public string Latitude { get; set; }
+            public string Longitude { get; set; }
+            public string Image { get; set; }
+            public string Remarks { get; set; }
+            public string CreatedBy { get; set; }
+            public DateTime CreatedDate { get; set; }
+        }
+
+        public class Handler : IRequestHandler<GetCheckInReportsQuery, PagedList<GetCheckInReportsResult>>
+        {
+            private readonly ArcanaDbContext _context;
+            public Handler(ArcanaDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<PagedList<GetCheckInReportsResult>> Handle(GetCheckInReportsQuery request, CancellationToken cancellationToken)
+            {
+                var adjustedDateTo = request.DateTo.AddDays(1);
+
+                var checkIn = _context.CheckIns
+                    .Include(c => c.Client)
+                        .ThenInclude(b => b.BusinessAddress)
+                    .Include(u => u.CreatedBy)
+                    .Where(t => t.CreatedDate >= request.DateFrom && t.CreatedDate < adjustedDateTo)
+                    .AsQueryable();
+
+                var result = checkIn
+                            .OrderByDescending(ck => ck.CreatedDate)
+                            .Select(ck => new GetCheckInReportsResult
+                            {
+                                BusinessName = ck.ClientId == null ? ck.BusinessNameOthers : ck.Client.BusinessName,
+                                FullName = ck.ClientId == null ? ck.FullNameOthers : ck.Client.Fullname,
+                                HouseNo = ck.ClientId == null ? null : ck.Client.BusinessAddress.HouseNumber,
+                                StreetName = ck.ClientId == null ? null : ck.Client.BusinessAddress.StreetName,
+                                Barangay = ck.ClientId == null ? ck.BarangayOthers : ck.Client.BusinessAddress.Barangay,
+                                City = ck.ClientId == null ? ck.CityOthers : ck.Client.BusinessAddress.City,
+                                Province = ck.ClientId == null ? ck.ProvinceOthers : ck.Client.BusinessAddress.Province,
+                                Latitude = ck.Latitude,
+                                Longitude = ck.Longitude,
+                                Image = ck.Image,
+                                Remarks = ck.Remarks,
+                                CreatedBy = ck.CreatedBy.Fullname,
+                                CreatedDate = ck.CreatedDate
+                            });
+
+                return await PagedList<GetCheckInReportsResult>.CreateAsync(result, request.PageNumber, request.PageSize);
+            }
+        }
+    }
+}
---
+++ RDF.Arcana.API/Features/Reports/CheckInReports.cs
@@ -0,0 +1,139 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Data;
+
+namespace RDF.Arcana.API.Features.Reports
+{
+    [Route("api/check-ins-reports"), ApiController]
+    public class CheckInReports : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CheckInReports(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckInsResult([FromQuery] CheckInReportsQuery query)
+        {
+            try
+            {
+                var result = await _mediator.Send(query);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public class CheckInReportsQuery : IRequest<IActionResult>
+        {
+            public int? AddedBy { get; set; }
+            public DateTime DateFrom { get; set; }
+            public DateTime DateTo { get; set; }
+        }
+
+        public class Handler : IRequestHandler<CheckInReportsQuery, IActionResult>
+        {
+            private readonly ArcanaDbContext _context;
+            public Handler(ArcanaDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IActionResult> Handle(CheckInReportsQuery request, CancellationToken cancellationToken)
+            {
+                var adjustedDateTo = request.DateTo.AddDays(1);
+
+                var query = _context.CheckIns
+                    .Include(c => c.Client)
+                        .ThenInclude(b => b.BusinessAddress)
+                    .Include(u => u.CreatedBy)
+                    .Where(c => c.CreatedDate >= request.DateFrom && c.CreatedDate < adjustedDateTo)
+                    .AsSplitQuery()
+                    .AsNoTracking();
+
+                var consolidate = await query.ToListAsync(cancellationToken);
+
+                using (var workbook = new XLWorkbook())
+                {
+                    var worksheet = workbook.Worksheets.Add("Check-In Reports");
+
+                    var headers = new List<string>
+                    {
+                        "User",
+                        "Visit Date",
+                        "Business Name",
+                        "Business Owner",
+                        "Barangay",
+                        "City",
+                        "Province",
+                        "Latitude",
+                        "Longitude"
+                    };
+
+                    var headerRange = worksheet.Range(worksheet.Cell(1, 1), worksheet.Cell(1, headers.Count));
+                    headerRange.Style.Fill.BackgroundColor = XLColor.FromHtml("#544d91");
+                    headerRange.Style.Font.Bold = true;
+                    headerRange.Style.Font.FontColor = XLColor.White;
+                    headerRange.Style.Border.OutsideBorder = XLBorderStyleValues.Thick;
+                    headerRange.Style.Border.OutsideBorderColor = XLColor.Black;
+                    headerRange.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                    for (var index = 0; index < headers.Count; index++)
+                    {
+                        worksheet.Cell(1, index + 1).Value = headers[index];
+                    }
+
+                    var evenRowColor = XLColor.FromHtml("#eae9f4");
+                    var oddRowColor = XLColor.FromHtml("#dcd9e9");
+
+                    for (var index = 0; index < consolidate.Count; index++)
+                    {
+                        var row = worksheet.Row(index + 2);
+
+                        var rowColor = index % 2 == 0 ? evenRowColor : oddRowColor;
+                        row.Style.Fill.BackgroundColor = rowColor;
+
+                        int rowNumber = index + 2;
+
+                        row.Cell(1).Value = consolidate[index].CreatedBy.Fullname;
+                        row.Cell(2).Value = consolidate[index].CreatedDate.ToString("MM/dd/yy HH:mm:ss");
+                        row.Cell(3).Value = consolidate[index].Client?.BusinessName ?? consolidate[index].BusinessNameOthers;
+                        row.Cell(4).Value = consolidate[index].Client?.Fullname ?? consolidate[index].FullNameOthers;
+                        row.Cell(5).Value = consolidate[index].Client?.BusinessAddress?.Barangay ?? consolidate[index].BarangayOthers;
+                        row.Cell(6).Value = consolidate[index].Client?.BusinessAddress?.City ?? consolidate[index].CityOthers;
+                        row.Cell(7).Value = consolidate[index].Client?.BusinessAddress?.Province ?? consolidate[index].ProvinceOthers;
+                        row.Cell(8).Value = consolidate[index].Latitude;
+                        row.Cell(9).Value = consolidate[index].Longitude;
+
+                        //for centering the numeric value for better readability
+                        for (int col = 1; col <= 45; col++)
+                        {
+                            var cell = row.Cell(col);
+                            if (decimal.TryParse(cell.Value.ToString(), out _))
+                            {
+                                cell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                            }
+                        }
+                    }
+
+                    worksheet.Columns().AdjustToContents();
+
+                    var stream = new MemoryStream();
+                    workbook.SaveAs(stream);
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    string fileName = $"CheckIns_Reports_{request.DateFrom:MMM d, yyyy}-{request.DateTo:MMM d, yyyy}.xlsx";
+                    return new FileStreamResult(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                    {
+                        FileDownloadName = fileName
+                    };
+                }
+
+            }
+        }
+    }
+}
