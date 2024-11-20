7de6d35836c08f25850b63fff413533090d296d8
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-11-20 16:57:54 +0800

    ongoing listing

---
+++ RDF.Arcana.API/Features/Listing Fee/GetListingFeeHistoryByClientId.cs
@@ -0,0 +1,48 @@
+﻿using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Data;
+
+namespace RDF.Arcana.API.Features.Listing_Fee
+{
+    public class GetListingFeeHistoryByClientId
+    {
+        public class GetListingFeeHistoryByClientIdQuery : IRequest<Result>
+        {
+            public int ClientId { get; set; }
+        }
+
+        public class GetListingFeeHistoryByClientIdResult
+        {
+            public string BusinessName { get; set; }
+            public decimal RemainingBalance { get; set; }
+            public ICollection<LFHistory> ListingHistory { get; set; }
+            public class LFHistory
+            {
+                public string PaymentType { get; set; }
+                public decimal Amount { get; set; }
+                public DateTime CreatedDate { get; set; }
+            }
+        }
+
+        //public class Handler : IRequestHandler<GetListingFeeHistoryByClientIdQuery, Result>
+        //{
+        //    private readonly ArcanaDbContext _context;
+        //    public Handler(ArcanaDbContext context)
+        //    {
+        //        _context = context;
+        //    }
+
+        //    public async Task<Result> Handle(GetListingFeeHistoryByClientIdQuery request, CancellationToken cancellationToken)
+        //    {
+        //        var listingFee = await _context.ListingFees
+        //            .Where(lf => lf.ClientId == request.ClientId &&
+        //                     lf.Status == Status.Approved)
+        //            .Select(x => new
+        //            {
+        //                BusinessName = x.Client.BusinessName,
+        //                TotalLf = x.Total,
+
+        //            });
+        //    }
+        //}
+    }
+}
